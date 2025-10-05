700a9bdd8b9c5066ec90acad1c8e054b8f9ff5bc
AeCw <aecbanana@outlook.com>
2025-10-05 12:55:51 +0800

    test

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
+  images: {
+    remotePatterns: [
       {
-        // Apply to all routes
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-        ],
+        protocol: 'https',
+        hostname: 'assets.ppy.sh',
+        port: '',
+        pathname: '/**',
       },
-    ];
+      {
+        protocol: 'https',
+        hostname: 'a.ppy.sh',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'preview.tryz.id.vn',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
